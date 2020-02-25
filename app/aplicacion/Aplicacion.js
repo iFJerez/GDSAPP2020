f3acbc96c2675da4726c309e69b86ff9e2cea8f8
mstone <mstone@gdsnet.com>
2020-02-25 15:25:11 -0300

    Update Aplicacion.js

--- app/aplicacion/Aplicacion.js
+++ app/aplicacion/Aplicacion.js
@@ -6,7 +6,7 @@ import AplicacionNavigation from './AplicacionNavigation'
 import ModalScreen from '../screens/ModalScreen'
 import HelpScreen from '../screens/HelpScreen'
 import Activity from '../screens/ActivityScreen'
-
+import FlashMessage from "react-native-flash-message";
 
 // Imports: Redux Actions
 import ActionCreators from '../redux/actions';
@@ -43,17 +43,11 @@ class Applicacion extends React.Component {
     return (
  
  <View style={{flex: 1}}>
-
-      <Text  style={{flex: 0.1, marginTop: 100}}> aplicacion : {this.state.dato} 
-      a continuacion {JSON.stringify(this.props.isRefresh)} 
-      </Text>
-
-
+      <FlashMessage position="top" />
         <AplicacionNavigation />
         <ModalScreen />
         <HelpScreen />
-        
-   
+
 
   
  </View>
