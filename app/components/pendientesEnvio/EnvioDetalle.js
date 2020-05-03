1582d496a6dcf4c13b9572faf43ea58aebc79ec7
santagli-gds <63793653+santagli-gds@users.noreply.github.com>
2020-05-03 11:14:21 -0300

    Se agrego EnvioDetalleCard para revision

--- app/components/pendientesEnvio/EnvioDetalle.js
+++ app/components/pendientesEnvio/EnvioDetalle.js
@@ -7,8 +7,10 @@ export default class PendienteEnvioBody extends Component {
   render() {
     const data = {
     "descSala": "Jumbo el Llano",
+     "numero": 1,
      "indicador": "Promocion",
-     "item": "Producto 1000cc",
+     "item": "Producto Ejecutado 1000cc",
+     "ean": "7878787839232",
      "accion": "Quiebre"}
     return (
       <View style={styles.container}>
